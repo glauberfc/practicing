55a3bfa4a7be5e22819e3227fec94240a922e748
Glauber Castro <gfc714@gmail.com>
2018-11-12 16:55:44 -0300

    Begin calling the Register Mutation

--- fullstack-graphql-airbnb-clone/packages/server/src/modules/user/forgotPassword/forgotPassword.test.ts
+++ fullstack-graphql-airbnb-clone/packages/server/src/modules/user/forgotPassword/forgotPassword.test.ts
@@ -1,78 +1,78 @@
-import { Connection } from "typeorm";
-import * as Redis from "ioredis";
-import * as faker from "faker";
+import { Connection } from 'typeorm'
+import * as Redis from 'ioredis'
+import * as faker from 'faker'
+import { passwordNotLongEnough } from '@abb/common'
 
-import { User } from "../../../entity/User";
-import { TestClient } from "../../../utils/TestClient";
-import { createForgotPasswordLink } from "../../../utils/createForgotPasswordLink";
-import { forgotPasswordLockAccount } from "../../../utils/forgotPasswordLockAccount";
-import { passwordNotLongEnough } from "../register/errorMessages";
-import { expiredKeyError } from "./errorMessages";
-import { forgotPasswordLockedError } from "../login/errorMessages";
-import { createTestConn } from "../../../testUtils/createTestConn";
+import { User } from '../../../entity/User'
+import { TestClient } from '../../../utils/TestClient'
+import { createForgotPasswordLink } from '../../../utils/createForgotPasswordLink'
+import { forgotPasswordLockAccount } from '../../../utils/forgotPasswordLockAccount'
+import { expiredKeyError } from './errorMessages'
+import { forgotPasswordLockedError } from '../login/errorMessages'
+import { createTestConn } from '../../../testUtils/createTestConn'
 
-let conn: Connection;
-export const redis = new Redis();
-faker.seed(Date.now() + 0);
-const email = faker.internet.email();
-const password = faker.internet.password();
-const newPassword = faker.internet.password();
+let conn: Connection
+export const redis = new Redis()
+faker.seed(Date.now() + 0)
+const email = faker.internet.email()
+const password = faker.internet.password()
+const newPassword = faker.internet.password()
 
-let userId: string;
+let userId: string
 beforeAll(async () => {
-  conn = await createTestConn();
+  conn = await createTestConn()
   const user = await User.create({
     email,
     password,
-    confirmed: true
-  }).save();
-  userId = user.id;
-});
+    confirmed: true,
+  }).save()
+  userId = user.id
+})
 
 afterAll(async () => {
-  conn.close();
-});
+  conn.close()
+})
 
-describe("forgot password", () => {
-  test("make sure it works", async () => {
-    const client = new TestClient(process.env.TEST_HOST as string);
+describe('forgot password', () => {
+  test('make sure it works', async () => {
+    const client = new TestClient(process.env.TEST_HOST as string)
 
     // lock account
-    await forgotPasswordLockAccount(userId, redis);
-    const url = await createForgotPasswordLink("", userId, redis);
+    await forgotPasswordLockAccount(userId, redis)
+    const url = await createForgotPasswordLink('', userId, redis)
 
-    const parts = url.split("/");
-    const key = parts[parts.length - 1];
+    const parts = url.split('/')
+    const key = parts[parts.length - 1]
 
     // make sure you can't login to locked account
     expect(await client.login(email, password)).toEqual({
       data: {
         login: [
           {
-            path: "email",
-            message: forgotPasswordLockedError
-          }
-        ]
-      }
-    });
+            path: 'email',
+            message: forgotPasswordLockedError,
+          },
+        ],
+      },
+    })
 
     // try changing to a password that's too short
-    expect(await client.forgotPasswordChange("a", key)).toEqual({
+    expect(await client.forgotPasswordChange('a', key)).toEqual({
       data: {
         forgotPasswordChange: [
           {
-            path: "newPassword",
-            message: passwordNotLongEnough
-          }
-        ]
-      }
-    });
+            path: 'newPassword',
+            message: passwordNotLongEnough,
+          },
+        ],
+      },
+    })
 
-    const response = await client.forgotPasswordChange(newPassword, key);
+    const response = await client.forgotPasswordChange(newPassword, key)
 
     expect(response.data).toEqual({
-      forgotPasswordChange: null
-    });
+      forgotPasswordChange: null,
+    })
 
     // make sure redis key expires after password change
     expect(
@@ -81,17 +81,17 @@ describe("forgot password", () => {
       data: {
         forgotPasswordChange: [
           {
-            path: "key",
-            message: expiredKeyError
-          }
-        ]
-      }
-    });
+            path: 'key',
+            message: expiredKeyError,
+          },
+        ],
+      },
+    })
 
     expect(await client.login(email, newPassword)).toEqual({
       data: {
-        login: null
-      }
-    });
-  });
-});
+        login: null,
+      },
+    })
+  })
+})
