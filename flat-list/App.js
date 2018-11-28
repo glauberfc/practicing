aa167662657c389d5960e0ac915ace261f524fb0
Glauber <glauber@MacBook-Pro-GlauberFC.local>
2018-11-28 14:35:26 -0300

    Fetch data from API and render FlatList

--- flat-list/App.js
+++ flat-list/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native'
 
-type Props = {}
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = async () => {
+    try {
+      const response = await fetch('https://randomuser.me/api?results=10')
+      const data = await response.json()
+      this.setState({ data: data.results })
+    } catch (error) {
+      console.log('Error on fetch data ---> ', error)
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <FlatList
-          data={[{ name: 'Fulano' }]}
+          data={this.state.data}
           keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
+          renderItem={({ item }) => <Text>{item.name.first}</Text>}
         />
       </View>
     )
@@ -21,6 +38,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 50,
     backgroundColor: '#F5FCFF',
   },
 })
