279c8233a3874a5b59ec0fc728527f5a2578a3b5
Glauber <glauber@MacBook-Pro-GlauberFC.local>
2018-12-22 00:48:47 -0300

    Sending Test Emails with Nodemailer

--- fullstack-graphql-airbnb-clone/packages/server/src/utils/sendEmail.ts
+++ fullstack-graphql-airbnb-clone/packages/server/src/utils/sendEmail.ts
@@ -1,22 +1,40 @@
-import * as SparkPost from "sparkpost";
-const client = new SparkPost(process.env.SPARKPOST_API_KEY);
+import * as nodemailer from 'nodemailer'
 
 export const sendEmail = async (recipient: string, url: string) => {
-  const response = await client.transmissions.send({
-    options: {
-      sandbox: true
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'mhmwgdhc5hauwlqz@ethereal.email',
+      pass: 'afs51QQ78jQkSpU8DW',
     },
-    content: {
-      from: "testing@sparkpostbox.com",
-      subject: "Confirm Email",
-      html: `<html>
+  })
+
+  // setup email data with unicode symbols
+  const mailOptions = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: recipient, // list of receivers
+    subject: 'Confirm Email ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html: `<html>
         <body>
-        <p>Testing SparkPost - the world's most awesomest email service!</p>
+        <p>Testing - the world's most awesomest email service!</p>
         <a href="${url}">confirm email</a>
         </body>
-        </html>`
-    },
-    recipients: [{ address: recipient }]
-  });
-  console.log(response);
-};
+        </html>`,
+  }
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message sent: %s', info.messageId)
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  })
+}
