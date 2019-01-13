0a79281e238e68f25cc1c3705bcdf06db813bc3f
Glauber Castro <gfc714@gmail.com>
2019-01-13 12:08:51 -0300

    Clear cookies

--- fullstack-graphql-airbnb-clone/packages/server/src/modules/user/logout/resolvers.ts
+++ fullstack-graphql-airbnb-clone/packages/server/src/modules/user/logout/resolvers.ts
@@ -1,21 +1,22 @@
-import { ResolverMap } from "../../../types/graphql-utils";
-import { removeAllUsersSessions } from "../../../utils/removeAllUsersSessions";
+import { ResolverMap } from '../../../types/graphql-utils'
+import { removeAllUsersSessions } from '../../../utils/removeAllUsersSessions'
 
 export const resolvers: ResolverMap = {
   Mutation: {
-    logout: async (_, __, { session, redis }) => {
-      const { userId } = session;
+    logout: async (_, __, { session, redis, res }) => {
+      const { userId } = session
       if (userId) {
-        removeAllUsersSessions(userId, redis);
+        removeAllUsersSessions(userId, redis)
         session.destroy(err => {
           if (err) {
-            console.log(err);
+            console.log(err)
           }
-        });
-        return true;
+        })
+        res.clearCookie('qid')
+        return true
       }
 
-      return false;
-    }
-  }
-};
+      return false
+    },
+  },
+}
