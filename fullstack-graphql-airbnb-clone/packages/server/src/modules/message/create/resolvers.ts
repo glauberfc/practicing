dc68bd103f944ae042ff1add54c8a8049be72537
Glauber Castro <gfc714@gmail.com>
2019-01-16 11:45:11 -0300

    GraphQL Subscriptions with GraphQL Yoga

--- fullstack-graphql-airbnb-clone/packages/server/src/modules/message/create/resolvers.ts
+++ fullstack-graphql-airbnb-clone/packages/server/src/modules/message/create/resolvers.ts
@@ -1,14 +1,19 @@
 import { ResolverMap } from '../../../types/graphql-utils'
 import { Message } from '../../../entity/Message'
+import { PUBSUB_NEW_MESSAGE } from '../shared/constants'
 
 export const resolvers: ResolverMap = {
   Mutation: {
-    createMessages: async (_, { message }, { session }) => {
+    createMessages: async (_, { message }, { session, pubsub }) => {
       // isAuthenticated(session)
-      await Message.create({
+      const dbMessage = await Message.create({
         ...message,
         userId: session.userId,
       }).save()
+
+      pubsub.publish(PUBSUB_NEW_MESSAGE, {
+        newMessage: dbMessage,
+      })
 
       return true
     },
