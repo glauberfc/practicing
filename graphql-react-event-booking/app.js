2acb469dd3be1c807ef189001bc8037395e44bf2
Glauber Castro <gfc714@gmail.com>
2019-02-19 09:52:24 -0300

    Schemas & Resolvers

--- graphql-react-event-booking/app.js
+++ graphql-react-event-booking/app.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, wolrld!')
-})
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => ['Romantic Cooking', 'Sailing', 'All-Night Coding'],
+      createEvent: args => args.name,
+    },
+    graphiql: true,
+  })
+)
 
 app.listen(3000)
