206311bee61ca04975b0142b9475ac79cbd62d78
Glauber Castro <gfc714@gmail.com>
2019-02-26 22:12:42 -0300

    First commit GoBarber

---
+++ gobarber/src/app/controllers/AvailableController.js
@@ -0,0 +1,8 @@
+const moment = require('moment')
+const { Appointment } = require('../models')
+
+class AvailableController {
+  async index(req, res) {}
+}
+
+module.exports = new AvailableController()
