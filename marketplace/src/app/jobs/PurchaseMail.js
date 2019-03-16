ca1579dd9a4fc02415fe637e05bd07ef814848df
Glauber Castro <gfc714@gmail.com>
2019-03-16 19:31:53 -0300

    Fix method name

--- marketplace/src/app/jobs/PurchaseMail.js
+++ marketplace/src/app/jobs/PurchaseMail.js
@@ -10,7 +10,7 @@ class PurchaseMail {
     const { ad, user, content } = job.data
 
     try {
-      await Mail.sendMaffil({
+      await Mail.sendMail({
         from: '"Marketplace" <marketplace@mail.com>',
         to: ad.author.email,
         subject: `Puchase request: "${ad.title}"`,
